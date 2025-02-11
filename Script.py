2b4dd067768e5967060f369b05ba194247bdc8bc
Alokhas <91585593+Alokhas@users.noreply.github.com>
2025-02-11 14:09:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ
 ✯ Mʏ Vᴇʀꜱɪᴏɴ: 4.5.0</b>"""
    
     SOURCE_TXT = """<b>NOTE:</b>
-- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href=https://gplinks.co/a5AcA>Mila</a>
+- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href=https://t.me/Mila_Movies_Bot>Mila</a>
 
 <b>ᴅᴇᴠ: <a herf=https://t.me/Prime_Alok>Alok</a></b>"""
 
